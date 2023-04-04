74ed164ee3f091d148f7b5a5e66f9185aafbed35
Charlotte-code9 <medallacharlotte7@gmail.com>
2023-04-05 00:22:12 +0800

    Added GameBoard, LeaderBorad and About

--- app/src/main/java/ph/stacktrek/novare/snakeandladder/medalla/sabellano/HomeFragment.kt
+++ app/src/main/java/ph/stacktrek/novare/snakeandladder/medalla/sabellano/HomeFragment.kt
@@ -1,10 +1,13 @@
 package ph.stacktrek.novare.snakeandladder.medalla.sabellano
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import ph.stacktrek.novare.snakeandladder.medalla.sabellano.databinding.FragmentHomeBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +24,8 @@ class HomeFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var binding: FragmentHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -33,8 +38,15 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.startGameButton.setOnClickListener {
+            val intent = Intent(activity, PlayerSelectionActivity::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
 
     companion object {
